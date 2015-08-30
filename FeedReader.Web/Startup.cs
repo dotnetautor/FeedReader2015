1dd8c33a6617627e935770d48d949046fd13445a
Matthias Fischer <matthias@dotnetautor.de>
2015-08-30 22:09:23 +0200

    inital check in

---
+++ FeedReader.Web/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.StaticFiles;
+
+namespace FeedReader.Web
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDirectoryBrowser();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+
+      app.UseFileServer(new FileServerOptions { EnableDirectoryBrowsing = true });
+    }
+  }
+}
